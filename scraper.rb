4334927bb135e19708608b23802e4a1c94228499
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-08 12:24:16 -0500

    add loading all books into local array

--- scraper.rb
+++ scraper.rb
@@ -2,4 +2,26 @@
 require 'httparty'
 require 'nokogiri'
 
-response = HTTParty.get('')
+books = []
+50.times do |i| 
+  response = HTTParty.get("https://books.toscrape.com/catalogue/page-#{i+1}.html")
+  document = Nokogiri::HTML4(response.body)
+
+  # if response.code == 200
+  #   puts "okay"
+  # else
+  #   puts "error"
+  # end
+
+  all_books = document.css('article.product_pod')
+  all_books.each { |book|
+    title = book.css('h3 a').first['title']
+    price = book.css('.price_color').text.delete('^0-9.')
+    availability = book.css('.availability').text.strip 
+    book = [title, price, availability]
+    # puts book
+    books << book
+  }
+end
+
+puts books.count
