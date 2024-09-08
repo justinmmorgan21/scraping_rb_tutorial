81441672e09012afa50c6e42256939be8a2d3662
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-08 12:38:00 -0500

    add writing to csv file

--- scraper.rb
+++ scraper.rb
@@ -1,27 +1,34 @@
 # tutorial from https://www.youtube.com/watch?v=GnVZexqtosc
 require 'httparty'
 require 'nokogiri'
+require 'csv'
 
-books = []
-50.times do |i| 
-  response = HTTParty.get("https://books.toscrape.com/catalogue/page-#{i+1}.html")
-  document = Nokogiri::HTML4(response.body)
+CSV.open(
+  'books.csv', # filename
+  'w+', # mode
+  write_headers: true, # **options
+  headers: %w[Title, Price, Availability]
+) do |csv|
 
-  # if response.code == 200
-  #   puts "okay"
-  # else
-  #   puts "error"
-  # end
+  50.times do |i| 
+    response = HTTParty.get("https://books.toscrape.com/catalogue/page-#{i+1}.html")
+    document = Nokogiri::HTML4(response.body)
 
-  all_books = document.css('article.product_pod')
-  all_books.each { |book|
-    title = book.css('h3 a').first['title']
-    price = book.css('.price_color').text.delete('^0-9.')
-    availability = book.css('.availability').text.strip 
-    book = [title, price, availability]
-    # puts book
-    books << book
-  }
+    # if response.code == 200
+    #   puts "okay"
+    # else
+    #   puts "error"
+    # end
+
+    all_books = document.css('article.product_pod')
+    all_books.each { |book|
+      title = book.css('h3 a').first['title']
+      price = book.css('.price_color').text.delete('^0-9.')
+      availability = book.css('.availability').text.strip 
+      book = [title, price, availability]
+      # puts book
+      csv << book
+    }
+  end
+
 end
-
-puts books.count
